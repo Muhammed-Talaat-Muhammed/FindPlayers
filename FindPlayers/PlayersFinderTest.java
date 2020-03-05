903aa7660e144def91eb7759c0c2976bac5a5b40
Muhammed-Talaat-Muhammed <61003316+Muhammed-Talaat-Muhammed@users.noreply.github.com>
2020-03-05 14:44:34 +0200

    Update PlayersFinderTest.java

--- FindPlayers/PlayersFinderTest.java
+++ FindPlayers/PlayersFinderTest.java
@@ -19,7 +19,7 @@ class PlayersFinderTest {
 	    assertArrayEquals(tester,test.findPlayers(tester1,0,10));
 		
 		tester1=new String[]{"8D88888J8L8E888", "88NKMG8N8E8JI88","888NS8EU88HN8EO",
-				             "LUQ888A8TH8OIH8","888QJ88R8SG88TY", "88ZQV88B88OUZ8O",
+				             "LUQ888A8TH8OIH8", "888QJ88R8SG88TY", "88ZQV88B88OUZ8O",
 				             "FQ88WF8Q8GG88B8", "8S888HGSB8FT8S8", "8MX88D88888T8K8", 
 				             "8S8A88MGVDG8XK8", "M88S8B8I8M88J8N", "8W88X88ZT8KA8I8",
 				             "88SQGB8I8J88W88", "U88H8NI8CZB88B8", "8PK8H8T8888TQR8" };
@@ -36,10 +36,10 @@ class PlayersFinderTest {
 		
 		assertArrayEquals(tester,test.findPlayers(tester1,0,6));
 		
-        try {
+
 		tester1=new String[]{""};//empty image
-		assertEquals("EMPTY IMG",test.findPlayers(tester1,0,6));}
-        catch(NullPointerException e) {System.out.println(e.getMessage());};//shows a EMPTY IMG message
+		assertEquals(null,test.findPlayers(tester1,0,6));
+
         
         tester1=new String[]{"rr0","hd0","dd1"};
         tester= new Point[0];
